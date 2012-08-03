7042c44c996f6d893d366c8d2abc277bf8455614
Joe Smith <yasumoto7@gmail.com>
2012-08-02 22:24:02 -0700

    Oh, you mean capitalization is _actually_ important.
    Signed-off-by: Joe Smith <yasumoto7@gmail.com>

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -38,6 +38,6 @@ RailsTutorialApp::Application.configure do
   # Speed up tests by lowering BCrypt's cost function.
   require 'bcrypt'
   silence_warnings do
-    BCrypt::Engine::DEFAULT_COST = Bcrypt::Engine::MIN_COST
+    BCrypt::Engine::DEFAULT_COST = BCrypt::Engine::MIN_COST
   end
 end
